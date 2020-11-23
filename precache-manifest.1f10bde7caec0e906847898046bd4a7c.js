9969f8ebe50c441bda51bddc12f8e4d8fc13dc29
Kamil_IT <kamillo.gronek@gmail.com>
2020-11-23 19:35:42 +0100

    Updates

---
+++ precache-manifest.1f10bde7caec0e906847898046bd4a7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd017cadff4307a25061336fcbe59e8",
+    "url": "/React-Pokemon-API-Pagination-Refs/index.html"
+  },
+  {
+    "revision": "88cde22576607ba61516",
+    "url": "/React-Pokemon-API-Pagination-Refs/static/css/2.99db5e02.chunk.css"
+  },
+  {
+    "revision": "bf62adf6a26a72fea172",
+    "url": "/React-Pokemon-API-Pagination-Refs/static/css/main.32a7627c.chunk.css"
+  },
+  {
+    "revision": "88cde22576607ba61516",
+    "url": "/React-Pokemon-API-Pagination-Refs/static/js/2.345388bd.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/React-Pokemon-API-Pagination-Refs/static/js/2.345388bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf62adf6a26a72fea172",
+    "url": "/React-Pokemon-API-Pagination-Refs/static/js/main.ec2ba2b7.chunk.js"
+  },
+  {
+    "revision": "fd3857ceb9e47909444c",
+    "url": "/React-Pokemon-API-Pagination-Refs/static/js/runtime-main.a8de93ba.js"
+  }
+]);
